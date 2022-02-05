5694bd14afd7150b1cde2d56b44e14c6718a9486
jozz024 <74272560+jozz024@users.noreply.github.com>
2022-02-05 08:37:18 -0600

    hopefully i didnt break anything with this commit

--- cogs/amiibots.py
+++ cogs/amiibots.py
@@ -1,87 +1,131 @@
 import difflib
-from nextcord.ext import commands
-from nextcord import slash_command
-from nextcord import Interaction, SlashOption
+from datetime import date
+
 import requests
 import util.amiibotsutil
 from dictionaries import CHARACTERS
+from nextcord import Interaction, SlashOption, slash_command
+from nextcord.ext import commands
 
-
+amiibotsutil = util.amiibotsutil.utilities()
 class amiibotsCog(commands.Cog):
-
-    def char(self, topbot, ruleset, character):
-        character_id = util.amiibotsutil.validatechar(character)
-        ruleset_id = util.amiibotsutil.getruleset(ruleset)
-        url = util.amiibotsutil.geturl(topbot, character_id, ruleset_id)
-        if character == 'overall':
-            output = (f"The {topbot} rated {ruleset.title()} amiibo are:```")
-        else: 
-            output = (f"The {topbot} rated {ruleset.title()} {character.title()} are:```")
+    def __init__(self, bot):
+        self.bot = bot
+    async def char(self, topbot, ruleset, character):
+        character_id = amiibotsutil.validatechar(character)
+        ruleset_id = amiibotsutil.getruleset(ruleset)
+        url = amiibotsutil.geturl(topbot, character_id, ruleset_id)
+        if character == "overall":
+            output = f"The {topbot} rated {ruleset.title()} amiibo are:```"
+        else:
+            output = f"The {topbot} rated {ruleset.title()} {character.title()} are:```"
         characterlink = list(requests.get(url).json()["data"])
-        if topbot == 'lowest':
+        if topbot == "lowest":
             characterlink = list(reversed(characterlink))
-        output = util.amiibotsutil.getoutput(characterlink, output)
+        output = await amiibotsutil.getoutput(characterlink, output, self.bot)
         return output
 
-    @slash_command(
-        name="topoverall"
-    )
+    @slash_command(name="topoverall")
     async def gettopthreenfp(
         self,
-        interaction: Interaction, 
-        ruleset = SlashOption(name="ruleset",choices={'vanilla':'vanilla','spirits':'spirits'}, description = "Ruleset you want data for."),
+        interaction: Interaction,
+        ruleset=SlashOption(
+            name="ruleset",
+            choices={"vanilla": "vanilla", "spirits": "spirits"},
+            description="Ruleset you want data for.",
+        ),
     ):
-        await interaction.send('Please wait while the data is being gathered for you.', ephemeral=True)
-        await interaction.edit_original_message(content = self.char("highest", ruleset, "overall"))
+        await interaction.send(
+            "Please wait while the data is being gathered for you.", ephemeral=True
+        )
+        await interaction.edit_original_message(
+            content=await self.char("highest", ruleset, "overall")
+        )
 
-    @slash_command(
-        name="botoverall"
-    )
+    @slash_command(name="botoverall")
     async def getbotthreenfp(
         self,
-        interaction: Interaction, 
-        ruleset = SlashOption(name="ruleset", choices={'vanilla':'vanilla','spirits':'spirits'}, description = "Ruleset you want the data for."),
+        interaction: Interaction,
+        ruleset=SlashOption(
+            name="ruleset",
+            choices={"vanilla": "vanilla", "spirits": "spirits"},
+            description="Ruleset you want the data for.",
+        ),
     ):
-        await interaction.send('Please wait while the data is being gathered for you.', ephemeral=True)
-        await interaction.edit_original_message(content = self.char("lowest", ruleset, "overall"))
+        await interaction.send(
+            "Please wait while the data is being gathered for you.", ephemeral=True
+        )
+        
+        await interaction.edit_original_message(
+            content=await self.char("lowest", ruleset, "overall")
+        )
 
     @slash_command(name="topchar")
     async def gettopthreenfpcharacter(
         self,
-        interaction: Interaction, 
-        character = SlashOption(name="character", description = "Character Name you want the data for."),
-        ruleset = SlashOption(name="ruleset", choices={'vanilla':'vanilla','spirits':'spirits'}, description = "Ruleset you want data for."),
+        interaction: Interaction,
+        character=SlashOption(
+            name="character", description="Character Name you want the data for."
+        ),
+        ruleset=SlashOption(
+            name="ruleset",
+            choices={"vanilla": "vanilla", "spirits": "spirits"},
+            description="Ruleset you want data for.",
+        ),
     ):
-        await interaction.send('Please wait while the data is being gathered for you.', ephemeral=True)
-        await interaction.edit_original_message(content = self.char("highest", ruleset, character))
+        await interaction.send(
+            "Please wait while the data is being gathered for you.", ephemeral=True
+        )
+        await interaction.edit_original_message(
+            content=await self.char("highest", ruleset, character)
+        )
 
     @slash_command(name="botchar")
     async def getbotthreenfpcharacter(
         self,
-        interaction: Interaction, 
-        character = SlashOption(name="character", description = "Character Name you want the data for."),
-        ruleset = SlashOption(name="ruleset", choices={'vanilla':'vanilla','spirits':'spirits'}, description = "Ruleset you want the data for."),
-):
-        await interaction.send('Please wait while the data is being gathered for you.', ephemeral=True)
-        await interaction.edit_original_message(content = self.char("lowest", ruleset, character))
+        interaction: Interaction,
+        character=SlashOption(
+            name="character", description="Character Name you want the data for."
+        ),
+        ruleset=SlashOption(
+            name="ruleset",
+            choices={"vanilla": "vanilla", "spirits": "spirits"},
+            description="Ruleset you want the data for.",
+        ),
+    ):
+        await interaction.send(
+            "Please wait while the data is being gathered for you.", ephemeral=True
+        )
+        await interaction.edit_original_message(
+            content=await self.char("lowest", ruleset, character)
+        )
 
     @slash_command(name="active")
-    async def activechar( 
+    async def activechar(
         self,
-        interaction: Interaction, 
-        character_name = SlashOption(name="character", description = "Character Name you want the data for."),
-        ruleset = SlashOption(name="ruleset", choices={'vanilla':'vanilla','spirits':'spirits'}, description = "Ruleset you want the data for."),
+        interaction: Interaction,
+        character_name=SlashOption(
+            name="character", description="Character Name you want the data for."
+        ),
+        ruleset=SlashOption(
+            name="ruleset",
+            choices={"vanilla": "vanilla", "spirits": "spirits"},
+            description="Ruleset you want the data for.",
+        ),
     ):
-        await interaction.send('Please wait while the data is being gathered for you.', ephemeral=True)
-        await interaction.edit_original_message(content = util.amiibotsutil.getactiveamiibo(character_name, ruleset))
-
+        await interaction.send(
+            "Please wait while the data is being gathered for you.", ephemeral=True
+        )
+        await interaction.edit_original_message(
+            content=amiibotsutil.getactiveamiibo(character_name, ruleset)
+        )
 
     @gettopthreenfpcharacter.on_autocomplete("character")
     async def autocompletechar(self, interaction: Interaction, character):
         test = difflib.get_close_matches(character, CHARACTERS, 10, 0.3)
         try:
             if character == test[0]:
-              test = [character]
+                test = [character]
         except IndexError:
             pass
         await interaction.response.send_autocomplete(test)
@@ -91,7 +135,7 @@ class amiibotsCog(commands.Cog):
         test = difflib.get_close_matches(character, CHARACTERS, 10, 0.3)
         try:
             if character == test[0]:
-              test = [character]
+                test = [character]
         except IndexError:
             pass
         await interaction.response.send_autocomplete(test)
@@ -101,10 +145,18 @@ class amiibotsCog(commands.Cog):
         test = difflib.get_close_matches(character, CHARACTERS, 10, 0.3)
         try:
             if character == test[0]:
-              test = [character]
+                test = [character]
         except IndexError:
             pass
         await interaction.response.send_autocomplete(test)
 
+#    @tasks.loop(hours = 24.0)
+#    async def updatedict(self):
+#        today = date.today()
+#        for users in self.user_dict:
+#            userinfo = requests.get(f'https://amiibots/api/user/by_user_id/{users[0]["id"]}').json()['data']
+#            users[0]['twitch_username'] = userinfo['twitch_username']
+#            users[0]['discord_id'] = userinfo['discord_id']
+#            users[1] = today.strftime("%Y-%m-%d")
 def setup(bot):
     bot.add_cog(amiibotsCog(bot))
--- util/amiibotsutil.py
+++ util/amiibotsutil.py
@@ -1,7 +1,8 @@
 from dictionaries import CHARACTER_NAME_TO_ID_MAPPING, RULSET_NAME_TO_ID_MAPPING
 import json
 import requests
-
+import datetime 
+from datetime import date
 skillsheet = requests.get(f"https://www.amiibots.com/api/spirit_skill").json()['data']
 
 with open('spirits.json', 'w+') as skill:
@@ -9,18 +10,35 @@ with open('spirits.json', 'w+') as skill:
 
 base_url = 'https://www.amiibots.com/api/amiibo'
 
-def validatechar(character):
+class utilities():
+    def __init__(self):
+        # The user dictionary for the upcoming api user feature
+        self.user_dict = {
+#            "0369ba61-e623-47e6-b13d-a13d6dcb8dd9": [
+#                {
+ #                   "id": "0369ba61-e623-47e6-b13d-a13d6dcb8dd9",
+ #                   "twitch_username": "jozz024",
+ #                   "discord_id": '554854714794049537',
+ #               },
+ #               "2022-2-5",
+  #          ]
+        }
+    def validatechar(self, character):
+            #makes the character id none for later use
             if character == 'overall':
                 character = 'None'
             else:
+                #if character isnt none, map character name to the character id
                 character = CHARACTER_NAME_TO_ID_MAPPING[character.lower()]
+            #returns the character id
             return character
 
-def getruleset(ruleset):
-    ruleset = RULSET_NAME_TO_ID_MAPPING[ruleset.lower()]
-    return ruleset
+    def getruleset(self, ruleset):
+        #maps the ruleset name to id
+        ruleset = RULSET_NAME_TO_ID_MAPPING[ruleset.lower()]
+        return ruleset
 
-def geturl(topbot, char_id, ruleset_id):
+    def geturl(self, topbot, char_id, ruleset_id):
         if topbot == 'highest' and char_id == 'None':
             url = base_url + '?per_page=15&ruleset_id=' + ruleset_id
         elif topbot == 'lowest' and char_id == 'None':
@@ -35,18 +53,26 @@ def geturl(topbot, char_id, ruleset_id):
             url = base_url + '?per_page=99999&ruleset_id=' + ruleset_id + '&playable_character_id=' + char_id
         return url
 
-def getoutput(characterlink, output):
+    async def getoutput(self, characterlink, output, bot):
         printed_amiibo_count = 0
         for amiibo in characterlink:
             if amiibo["total_matches"] >= 30:
                 printed_amiibo_count += 1
+
                 if amiibo['ruleset_id'] == '328d8932-456f-4219-9fa4-c4bafdb55776':
-                    output += f"\n{printed_amiibo_count:>2}.) {amiibo['name']:^10} | {amiibo['attack_stat']}/{amiibo['defense_stat']} | {round(amiibo['rating'], 2):0^5} | {int(amiibo['wins'])}-{int(amiibo['losses'])}"
+                    if self.user_dict.get(amiibo['user']) != None:
+                        output += f"\n{printed_amiibo_count:>2}.) {amiibo['name']:^10} | {amiibo['attack_stat']}/{amiibo['defense_stat']} | {round(amiibo['rating'], 2):0^5} | {int(amiibo['wins'])}-{int(amiibo['losses'])} | {await self.getusername(amiibo['user'], bot)}"
+                    else:
+                        output += f"\n{printed_amiibo_count:>2}.) {amiibo['name']:^10} | {amiibo['attack_stat']}/{amiibo['defense_stat']} | {round(amiibo['rating'], 2):0^5} | {int(amiibo['wins'])}-{int(amiibo['losses'])}"
+
                     for spirits in amiibo['spirit_skill_ids']:
-                        output += f"\n    -{getskills(spirits)}"
+                        output += f"\n    -{self.getskills(spirits)}"
                     output += '\n-----------------------------'
                 else:
-                    output += f"\n{printed_amiibo_count:>2}.) {amiibo['name']:^10} | {round(amiibo['rating'], 2):0^5} | {int(amiibo['wins'])}-{int(amiibo['losses'])}"
+                    if self.user_dict.get(amiibo['user']) != None:
+                        output += f"\n{printed_amiibo_count:>2}.) {amiibo['name']:^10} | {round(amiibo['rating'], 2):0^5} | {int(amiibo['wins'])}-{int(amiibo['losses'])} | {await self.getusername(amiibo['user'], bot)}"
+                    else:
+                        output += f"\n{printed_amiibo_count:>2}.) {amiibo['name']:^10} | {round(amiibo['rating'], 2):0^5} | {int(amiibo['wins'])}-{int(amiibo['losses'])}"
 
             if printed_amiibo_count >= 10:
                 break
@@ -56,23 +82,47 @@ def getoutput(characterlink, output):
             output = "No 30+ game amiibo for this character"
         return output
 
-def getskills(skill_id):
+    def getskills(self, skill_id):
          with open('spirits.json', 'r') as skill:
             skillss = json.load(skill)
             for skills in skillss:
                 if skills['id'] == skill_id:
                     return skills['name']
 
-def getactiveamiibo(character, ruleset):
-    character_id = validatechar(character)
-    ruleset_id = getruleset(ruleset)
-    url = geturl('active', character_id, ruleset_id)
-    characterlink = requests.get(url).json()['data']
-    amiiboactive = 0
-    for amiibo in characterlink:
-        if amiibo["is_active"] == True:
+    def getactiveamiibo(self, character, ruleset):
+        character_id = self.validatechar(character)
+        ruleset_id = self.getruleset(ruleset)
+        url = self.geturl('active', character_id, ruleset_id)
+        characterlink = requests.get(url).json()['data']
+        amiiboactive = 0
+        for amiibo in characterlink:
+            if amiibo["is_active"] == True:
                 amiiboactive = amiiboactive + 1
-    if character != None:
+        if character != None:
             return f"There are currently {amiiboactive} {character.title()} amiibo active in {ruleset}."
-    else: 
+        else: 
             return f"There are currently {amiiboactive} amiibo active in {ruleset}."
+            
+    def getUserById(self, user_id):
+        user_not_in_cache_or_outdated = user_id not in self.user_dict or date.today().strftime("%Y-%m-%d") != self.user_dict[user_id][1]
+        if user_not_in_cache_or_outdated:
+            userinfo = requests.get(f'https://amiibots/api/user/by_user_id/{user_id}').json()
+            self.user_dict[user_id] = [userinfo, date.today()]
+        else:
+            userinfo = self.user_dict[user_id]
+        return userinfo
+
+    async def getusername(self, user_id, bot):
+        user_not_in_cache_or_outdated = user_id not in self.user_dict or date.today().strftime("%Y-%m-%d") != self.user_dict[user_id][1]
+        if not user_not_in_cache_or_outdated:
+            userinfo = self.user_dict[user_id]
+        else:
+        #    userinfo = requests.get(f'https://amiibots/api/user/by_user_id/{user_id}').json()
+        #    self.user_dict[user_id] = [userinfo, date.today()]
+            userinfo = self.user_dict[user_id]
+
+        if userinfo[0]['discord_id'] != None:
+            user = await bot.fetch_user(int(userinfo[0]['discord_id']))
+            return user.name
+        else:
+            return userinfo[0]['twitch_username']
